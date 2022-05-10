cb468a276c5083c0a1845b10bbcf9b584f2c4d4a
Hakim0611 <hakim0611066@gmail.com>
2022-05-10 08:00:44 +0500

    10.05 8.00

--- src/js/script.js
+++ src/js/script.js
@@ -31,4 +31,18 @@ $(document).ready(function () {
         .addClass("catalog__content-active");
     }
   );
+
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on("click", function (e) {
+        e.preventDefault();
+        $(".catalog-item__content")
+          .eq(i)
+          .toggleClass("catalog-item__content-active");
+        $(".catalog-item__list").eq(i).toggleClass("catalog-item__list-active");
+      });
+    });
+  }
+  toggleSlide(".catalog-item__link");
+  toggleSlide(".catalog-item__back");
 });
